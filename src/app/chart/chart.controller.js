cdd5eba1f9c7fd2f62b72b23ac3d5bd832cf3f67
Sugan Krishnan <rgk.sugan@gmail.com>
2015-05-20 15:56:23 +0530

    few more charts

--- src/app/chart/chart.controller.js
+++ src/app/chart/chart.controller.js
@@ -1,8 +1,27 @@
-angular.module('tnstats').controller('ChartCtrl', function ($stateParams, $http) {
+angular.module('tnstats').controller('ChartCtrl', function ($stateParams, $http, $scope, $timeout) {
   'use strict';
 
   $http.get('assets/data/' + $stateParams.categoryID + '_' + $stateParams.subcategoryID + '.json').then(function (response) {
-    console.log(response.data);
+
+    var chart1Data = _.map(response.data, function (datum) {
+      return [datum.Religion, datum['Persons in Tamil Nadu']];
+    });
+
+    var chart2Data = _.map(response.data, function (datum) {
+      return [datum.Religion, datum['All India']];
+    });
+
+    var chart3Data = _.map(response.data, function (datum) {
+      return datum['Variation Since 1991-2001'];
+    });
+
+    var religions = _.pluck(response.data, 'Religion');
+    var variation = _.pluck(response.data, 'Variation Since 1991-2001');
+
+    $scope.data = response.data;
+
+    console.log(JSON.stringify($scope.data), JSON.stringify(chart1Data));
+
     $('.chart').highcharts({
       chart: {
         plotBackgroundColor: null,
@@ -10,29 +29,89 @@ angular.module('tnstats').controller('ChartCtrl', function ($stateParams, $http)
         plotShadow: false
       },
       title: {
-        text: 'Browser market shares at a specific website, 2014'
+        text: 'Population by Religion - Tamil Nadu'
       },
       tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        pointFormat: '{series.name}: <b>{point.percentage:.2f}%</b>'
       },
       plotOptions: {
         pie: {
           allowPointSelect: true,
           cursor: 'pointer',
           dataLabels: {
-            enabled: true,
-            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-            style: {
-              color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-            }
-          }
+            enabled: false
+          },
+          showInLegend: true
         }
+      },
+      credits: {
+        enabled: false
       },
       series: [{
         type: 'pie',
-        name: 'Browser share',
-        data: response.data
+        name: 'Population',
+        data: chart1Data
       }]
     });
+
+    $('.chart1').highcharts({
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false
+      },
+      title: {
+        text: 'Population by Religion - All India'
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.2f}%</b>'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: false
+          },
+          showInLegend: true
+        }
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+        type: 'pie',
+        name: 'Population',
+        data: chart2Data
+      }]
+    });
+
+    $('.chart2').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Variation since 2001'
+        },
+        xAxis: {
+            categories: religions
+        },
+        credits: {
+            enabled: false
+        },
+        legend: {
+            enabled: false
+        },
+        series: [{
+            data: chart3Data
+        }]
+    });
+
+    $timeout(function () {
+      $('#dataTable').DataTable({
+        paginate: false
+      });
+    });
+
   });
 });
